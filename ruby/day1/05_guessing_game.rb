4e8ca007021add4761aab3a7f6c7ea572dc06ef8
Christopher Brown <christopher.p.brown@gmail.com>
2017-03-27 19:14:04 -0600

    :lipstick: :books:

--- ruby/day1/05_guessing_game.rb
+++ ruby/day1/05_guessing_game.rb
@@ -5,7 +5,7 @@ guess = gets.to_i
 
 unless guess.eql? secret
   puts "Too #{guess > secret ? 'high' : 'low'}!"
-  guess = gets.to_i
+  guess = gets.to_i # do..while might remove this repetition?
 end
 
 puts 'Winner winner, tofu dinner!'
